4e59b2a36ac14a226b83c591334e1441ad840d5d
Yu-Neng Wang <wynwyn@stanford.edu>
2023-06-27 14:46:03 -0700

    Update readme and setup

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(
     version = '0.1.0',
     description = 'Spatial Analog Abstraction',
     packages=['ark'],
-    install_requires=['pyspice', 'matplotlib', 'numpy']
+    install_requires=['pyspice', 'matplotlib', 'numpy', 'pysmt']
 )

fc4e422cd750597b25b89eb11d9d80658b203074
Yu-Neng Wang <wynwyn@stanford.edu>
2023-09-28 14:46:21 -0700

    Update experiment script and remove todos (merged)

--- run_obc.sh
+++
@@ -1,9 +0,0 @@
-echo "Running obc example, compare the correct rate of ideal and mismatched implementation"
-echo "obc ideal"
-python examples/obc.py  --n_cycle 5 # -p
-echo "obc ideal+tol"
-python examples/obc.py  --n_cycle 5 --atol 0.1
-echo "obc offset"
-python examples/obc.py  --n_cycle 5 --offset_rstd 0.01 # -p
-echo "obc offset+tol"
-python examples/obc.py  --n_cycle 5 --offset_rstd 0.01 --atol 0.1

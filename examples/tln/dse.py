afe73ee825022a659ae492378125d107187a8ffe
Yu-Neng Wang <wynwyn@stanford.edu>
2023-10-30 15:03:14 -0700

    Add checkpoint inspecting and simulation script

--- examples/tln/dse.py
+++ examples/tln/dse.py
@@ -9,21 +9,32 @@ from typing import Callable
 
 import matplotlib.pyplot as plt
 import numpy as np
-from puf import SwitchableStarPUF, single_bit_flip_test, single_bit_flipped_neighbors
+from puf import (
+    PUF,
+    SwitchableStarPUF,
+    single_bit_flip_test,
+    single_bit_flipped_neighbors,
+)
 from spec import mm_tln_spec
 
+from ark.optimization.optimizer import BaseOptimizer
 from ark.optimization.sa import SimulatedAnnealing
 
 
 def sim_and_calc_flip_prob(
-    puf: SwitchableStarPUF, inst_id: int, center_chls: list[int]
-) -> list[list[float]]:
+    puf: SwitchableStarPUF,
+    inst_id: int,
+    center_chls: list[int],
+    return_crps: bool = False,
+) -> list[list[float]] | dict[int, np.ndarray]:
     """Simulate a puf instance and calculate the single-bit flipping probability test restult.
 
     Args:
         puf (SwitchableStarPUF): The PUF description.
         inst_id (int): The instance id to simulate.
         center_chls (list[int]): challenges to be flipped in the test.
+        return_crps (bool, optional): Return the challenge-response pairs instead of
+        the flipping probability. Defaults to False.
 
     Returns:
         list[list[float]]: (n_chl_bit, n_rsp_bit) Flipping probability of each
@@ -45,6 +56,8 @@ def sim_and_calc_flip_prob(
         crps=crps,
         center_chls=center_chls,
     )
+    if return_crps:
+        return crps
     return flipped_prob
 
 
@@ -56,6 +69,7 @@ def evaluate_puf_single_bit_flip(
     n_inst: int,
     n_core: int = 1,
     plot: bool = False,
+    return_crps: bool = False,
 ) -> float:
     """Evaluate the cost of a PUF by single-bit flipping probability test.
 
@@ -66,6 +80,8 @@ def evaluate_puf_single_bit_flip(
         n_inst (int): number of instances to simulate.
         n_core (int, optional): number of core to use. Defaults to 1.
         plot (bool, optional): plot the test results. Defaults to False.
+        return_crps: (bool, optional): return the challenge-response pairs
+        instead of the aggregated cost. Defaults to False.
 
     Returns:
         float: cost
@@ -80,16 +96,20 @@ def evaluate_puf_single_bit_flip(
     if n_core == 1:
         flipped_probs = []
         for inst_id, center_chls in enumerate(center_chlss):
-            flipped_probs.append(sim_and_calc_flip_prob(puf, inst_id, center_chls))
+            flipped_probs.append(
+                sim_and_calc_flip_prob(puf, inst_id, center_chls, return_crps)
+            )
     else:
         with Pool(n_core) as pool:
             flipped_probs = pool.starmap(
                 sim_and_calc_flip_prob,
                 [
-                    (puf, inst_id, center_chls)
+                    (puf, inst_id, center_chls, return_crps)
                     for inst_id, center_chls in enumerate(center_chlss)
                 ],
             )
+    if return_crps:  # the "flipped_probs" are actually raw crps
+        return flipped_probs
     dist_to_ideal = np.abs(np.array(flipped_probs) - 0.5)
     cost = np.min(
         [
@@ -102,7 +122,7 @@ def evaluate_puf_single_bit_flip(
     if plot:
         plt.title(f"Avg Distance to 0.5 vs Time, Tot Cost={cost:.4f}")
         for bit_pos, prob in enumerate(np.mean(dist_to_ideal, axis=0)):
-            plt.plot(time_points, prob, label=f"Bit {bit_pos}")
+            plt.plot(puf.time_points, prob, label=f"Bit {bit_pos}")
     return cost
 
 
@@ -204,59 +224,27 @@ def save_params(
     pickle.dump(params, open(f"{save_prefix}.pkl", "wb"))
 
 
-if __name__ == "__main__":
-    parser = ArgumentParser()
-    parser.add_argument("-c", "--config", type=str, required=True)
-    parser.add_argument(
-        "-g",
-        "--greedy",
-        action="store_true",
-        help="Use greedy mode of the simulated annealing engine, i.e., only accept a \
-            solution when the cost is strictly smaller.",
-    )
-    parser.add_argument(
-        "-w",
-        "--wandb",
-        action="store_true",
-        help="Use weight and bias for real time monitoring.",
-    )
-    args = parser.parse_args()
+def setup_puf(config: dict) -> tuple[PUF, dict]:
+    """Setup the PUF according to the config.
 
-    with open(args.config, "r") as f:
-        config = json.load(f)
+    Args:
+        config (dict): PUF config.
 
-    # PUD and simulation parameters
-    n_bits, line_len, n_inst = config["n_bits"], config["line_len"], config["n_inst"]
-    n_core = config["n_core"]
-    center_chls_size = config["center_chls_size"]
-    n_time_point, window_size = config["n_time_point"], config["window_size"]
-    perturb_all, increment = config["perturb_all"], config["increment"]
+    Returns:
+        SwitchableStarPUF: the PUF description.
+    """
+    np.random.seed(config["seed"])
+    n_bits = config["n_bits"]
+    line_len = config["line_len"]
+    n_inst = config["n_inst"]
+    n_time_point = config["n_time_point"]
     rand_init = config["rand_init"]
-    checkpoint_n_inst = config["checkpoint_n_inst"]
-    checkpoint_center_chls_size = config["checkpoint_center_chls_size"]
-    seed = config["seed"]
+    if "checkpoint" in config:
+        checkpoint = pickle.load(open(config["checkpoint"], "rb"))
+    else:
+        checkpoint = None
     time_end = config["time_end"]
-    use_wandb = args.wandb
-    greedy = args.greedy
 
-    # Optimizer parameters
-    temperature = config["temperature"]
-    frozen_temp = config["frozen_temp"]
-    temp_decay = config["temp_decay"]
-    inner_iteration = config["inner_iteration"]
-    checkpoint_dir = f"_run_{time.strftime('%Y%m%d-%H%M%S')}_{time.time():.0f}"
-    config["checkpoint_dir"] = checkpoint_dir
-    os.mkdir(checkpoint_dir)
-    print(f"Checkpoints store at {checkpoint_dir}")
-
-    optimizer = SimulatedAnnealing(
-        temperature=temperature,
-        frozen_temp=frozen_temp,
-        temp_decay=temp_decay,
-        inner_iteraion=inner_iteration,
-    )
-    np.random.seed(seed)
-    n_bits = n_bits
     time_range = [0, time_end]
     time_points = np.linspace(*time_range, n_time_point, endpoint=True)
     ss_puf = SwitchableStarPUF(
@@ -266,8 +254,15 @@ if __name__ == "__main__":
         spec=mm_tln_spec,
         time_points=time_points,
     )
-
-    if rand_init:
+    if checkpoint:
+        (
+            middle_cap_param,
+            middle_edge_param,
+            branch_v_param,
+            branch_i_param,
+            branch_e_param,
+        ) = checkpoint
+    elif rand_init:
         middle_cap_param = {
             "c": rand_lc(),
             "g": rand_rg(),
@@ -306,14 +301,6 @@ if __name__ == "__main__":
         branch_v_param = [vnode_param.copy() for _ in range(ss_puf.branch_n_nodes)]
         branch_i_param = [inode_param.copy() for _ in range(ss_puf.branch_n_nodes)]
         branch_e_param = [et_param.copy() for _ in range(ss_puf.branch_n_edges)]
-    ss_puf.set_circuit_param(
-        middle_cap_param,
-        middle_edge_param,
-        branch_v_param,
-        branch_i_param,
-        branch_e_param,
-    )
-    ss_puf.sample_instances(n_inst=n_inst)
 
     init_sol = (
         middle_cap_param,
@@ -322,10 +309,52 @@ if __name__ == "__main__":
         branch_i_param,
         branch_e_param,
     )
+    ss_puf.set_circuit_param(*init_sol)
+    ss_puf.sample_instances(n_inst=n_inst)
+    return ss_puf, init_sol
 
+
+def setup_optimization(config: dict, puf: PUF) -> BaseOptimizer:
+    """Setup the optimizer according to the config.
+
+    Args:
+        config (dict): optimizer config.
+        puf (PUF): the PUF description.
+    Returns:
+        BaseOptimizer: the optimizer.
+    """
+    # PUD parameters
+    n_inst = config["n_inst"]
+    center_chls_size = config["center_chls_size"]
+
+    # Optimizer parameters
+    temperature = config["temperature"]
+    frozen_temp = config["frozen_temp"]
+    temp_decay = config["temp_decay"]
+    inner_iteration = config["inner_iteration"]
+    checkpoint_dir = f"_run_{time.strftime('%Y%m%d-%H%M%S')}_{time.time():.0f}"
+    config["checkpoint_dir"] = checkpoint_dir
+    os.mkdir(checkpoint_dir)
+    json.dump(config, open(f"{checkpoint_dir}/config.json", "w"), indent=4)
+    print(f"Checkpoints store at {checkpoint_dir}")
+
+    # Evaluation function parameters
+    window_size = config["window_size"]
+    n_core = config["n_core"]
+    checkpoint_n_inst = config["checkpoint_n_inst"]
+    checkpoint_center_chls_size = config["checkpoint_center_chls_size"]
+    perturb_all = config["perturb_all"]
+    increment = config["increment"]
+
+    optimizer = SimulatedAnnealing(
+        temperature=temperature,
+        frozen_temp=frozen_temp,
+        temp_decay=temp_decay,
+        inner_iteraion=inner_iteration,
+    )
     eval_fn = ft.partial(
         evaluate_puf_single_bit_flip,
-        puf=ss_puf,
+        puf=puf,
         center_chls_size=center_chls_size,
         n_inst=n_inst,
         window_size=window_size,
@@ -333,7 +362,7 @@ if __name__ == "__main__":
     )
     eval_fn_more_sample = ft.partial(
         evaluate_puf_single_bit_flip,
-        puf=ss_puf,
+        puf=puf,
         center_chls_size=checkpoint_center_chls_size,
         n_inst=checkpoint_n_inst,
         window_size=window_size,
@@ -341,11 +370,39 @@ if __name__ == "__main__":
         n_core=n_core,
     )
     neighbor_fn = ft.partial(
-        perturb_attr, puf=ss_puf, perturb_all=perturb_all, increment=increment
+        perturb_attr, puf=puf, perturb_all=perturb_all, increment=increment
     )
     check_point_fn = ft.partial(
         save_params, eval_fn_more_samples=eval_fn_more_sample, save_dir=checkpoint_dir
     )
+    return optimizer, eval_fn, neighbor_fn, check_point_fn
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("-c", "--config", type=str, required=True)
+    parser.add_argument(
+        "-g",
+        "--greedy",
+        action="store_true",
+        help="Use greedy mode of the simulated annealing engine, i.e., only accept a \
+            solution when the cost is strictly smaller.",
+    )
+    parser.add_argument(
+        "-w",
+        "--wandb",
+        action="store_true",
+        help="Use weight and bias for real time monitoring.",
+    )
+    args = parser.parse_args()
+
+    with open(args.config, "r") as f:
+        config = json.load(f)
+    use_wandb = args.wandb
+    greedy = args.greedy
+
+    ss_puf, init_sol = setup_puf(config)
+    optimizer, eval_fn, neighbor_fn, check_point_fn = setup_optimization(config, ss_puf)
 
     final_params = optimizer.optimize(
         init_sol=init_sol,
@@ -357,4 +414,3 @@ if __name__ == "__main__":
         check_point_func=check_point_fn,
         greedy=greedy,
     )
-    # optimizer.visualize_log()

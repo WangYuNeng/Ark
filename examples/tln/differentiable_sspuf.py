Add lc values as trainable parameters - Also fix logistic numerical instability issue

examples/tln/differentiable_sspuf.py
@@ -18,9 +18,13 @@ class SwitchableStarPUF(eqx.Module):
     - n_branch: # of branches to switch
     - line_len: # of LC section in each branch
     - n_order: # of order to approximate the matrix exponential
-    - gm_c: Gm of integrators associated with capacitors
-    - gm_l: Gm of integrators associated with inductors
-    - lc_val: The value of the LC components
+    - gm_c: Trainable Gm of integrators associated with capacitors
+    - gm_l: Trainable Gm of integrators associated with inductors
+    - c_val: Trainable weights for C components
+    (c_val[-1] is the middle capacitor)
+    - l_val: Trainable weights for L components
+    - lc_val_base: The base value of the LC components
+    (l, c = lc_val_base * lc_val)
     - lds_a: The A matrix in the state-space representation of the
     SwithcableStar.
     - t: The time point of the output
@@ -32,7 +36,9 @@ class SwitchableStarPUF(eqx.Module):
     lds_a_shape: tuple[int, int]
     gm_c: jax.Array
     gm_l: jax.Array
-    lc_val: int = 1e-9
+    c_val: jax.Array
+    l_val: jax.Array
+    lc_val_base: int = 1e-9
     pulse_amplitude: float = 1.0
     pulse_t1: float = 0.5e-9
     pulse_t2: float = 1.5e-9
@@ -66,10 +72,14 @@ class SwitchableStarPUF(eqx.Module):
                 high=1.5,
                 size=(line_len, 2),
             )
+            self.c_val = np.random.uniform(low=0.5, high=1.5, size=(line_len + 1))
+            self.l_val = np.random.uniform(low=0.5, high=1.5, size=(line_len))
 
         else:
             self.gm_c = np.ones(shape=(line_len, 2))
             self.gm_l = np.ones(shape=(line_len, 2))
+            self.c_val = np.ones(shape=(line_len + 1))
+            self.l_val = np.ones(shape=(line_len))
 
     def __call__(self, switch: jax.Array, mismatch: jax.Array, t: jax.typing.DTypeLike):
         """Compute the analog PUF response at time t.
@@ -107,7 +117,7 @@ class SwitchableStarPUF(eqx.Module):
             a_sw_mm = a_sw_mm.at[0, 1 + i * self.line_len * 2].multiply(sw_val)
 
         b_mat = jnp.zeros(shape=(lds_a.shape[0], 1))
-        b_mat = b_mat.at[0, 0].set(1 / self.lc_val)
+        b_mat = b_mat.at[0, 0].set(1 / self.lc_val_base * self.c_val[-1])
         c_mat = jnp.zeros(shape=(1, lds_a.shape[0]))
         c_mat = c_mat.at[0, 0].set(1)
 
@@ -153,12 +163,14 @@ class SwitchableStarPUF(eqx.Module):
             for j in range(sub_mat_len - 1):
                 if j % 2 == 0:
                     c_idx = j // 2
-                    a_i = a_i.at[j, j + 1].set(-self.gm_c[c_idx, 1])
-                    a_i = a_i.at[j + 1, j].set(self.gm_c[c_idx, 0])
+                    a_i = a_i.at[j, j + 1].set(-self.gm_c[c_idx, 1] / self.l_val[c_idx])
+                    a_i = a_i.at[j + 1, j].set(self.gm_c[c_idx, 0] / self.c_val[c_idx])
                 else:
                     l_idx = j // 2 + 1
-                    a_i = a_i.at[j, j + 1].set(-self.gm_l[l_idx, 1])
-                    a_i = a_i.at[j + 1, j].set(self.gm_l[l_idx, 0])
+                    a_i = a_i.at[j, j + 1].set(
+                        -self.gm_l[l_idx, 1] / self.c_val[l_idx - 1]
+                    )
+                    a_i = a_i.at[j + 1, j].set(self.gm_l[l_idx, 0] / self.l_val[l_idx])
             return a_i
 
         ai_row_mats = [ith_diag_sub_mat() for _ in range(n_branch)]
@@ -173,10 +185,10 @@ class SwitchableStarPUF(eqx.Module):
         mid_to_branch_col = jnp.zeros(shape=(sub_mat_len * n_branch, 1))
         for i in range(n_branch):
             mid_to_branch_row = mid_to_branch_row.at[0, i * sub_mat_len].set(
-                -self.gm_l[0, 1]
+                -self.gm_l[0, 1] / self.c_val[-1]
             )
             mid_to_branch_col = mid_to_branch_col.at[i * sub_mat_len, 0].set(
-                self.gm_l[0, 0]
+                self.gm_l[0, 0] / self.l_val[0]
             )
 
         A_1_to_n = jnp.vstack([mid_to_branch_row] + ai_row_mats)
@@ -186,7 +198,7 @@ class SwitchableStarPUF(eqx.Module):
         )
 
         # Scale by LC values
-        LC_mat = jnp.eye(A_mat.shape[0]) / self.lc_val
+        LC_mat = jnp.eye(A_mat.shape[0]) / self.lc_val_base
 
         lds_a = jnp.matmul(LC_mat, A_mat)
         return lds_a

examples/tln/train.py
@@ -23,6 +23,7 @@ parser.add_argument("--n_order", type=int, default=40)
 parser.add_argument("--readout_time", type=float, default=10e-9)
 parser.add_argument("--rand_init", action="store_true")
 parser.add_argument("--batch_size", type=int, default=256)
+parser.add_argument("--inst_per_branch", type=int, default=1)
 parser.add_argument("--steps", type=int, default=200)
 parser.add_argument("--print_every", type=int, default=1)
 parser.add_argument("--logistic_k", type=float, default=40)
@@ -61,7 +62,7 @@ def shifted_sign(x):
 
 def logistic(x, k=10.0):
     """Smooth approximation of the ideal ADC with the logistic function."""
-    return 1.0 / (1.0 + jnp.exp(-x * k))
+    return 0.5 * (jnp.tanh(x * k / 2) + 1)
 
 
 def diff(model, switch, mismatch, t):
@@ -86,7 +87,29 @@ def random_chls_and_mismatch(batch_size, n_branch, lds_a_shape, readout_time):
         yield switches, mismatch, t
 
 
-def bf_chls(batch_size, n_branch, lds_a_shape, readout_time):
+def bf_chls(
+    batch_size: int,
+    inst_per_batch: int,
+    n_branch: int,
+    lds_a_shape: tuple[int, int],
+    readout_time: float,
+) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
+    """Bit-flipping test training data generator.
+
+    Args:
+        batch_size (int): size of the batch
+        inst_per_batch (int): # of instance (mismatch)to sample per batch
+        (batch_size // inst_per_batch should an integer >> inst_per_batch)
+        n_branch (int): # of branches in the star PUF
+        lds_a_shape (tuple[int, int]): shape of the PUF matrix
+        readout_time (float): readout time of the PUF
+
+    Yields:
+        switches (np.ndarray): batch of switches
+        mismatch (np.ndarray): batch of mismatch samples
+        t (np.ndarray): batch of readout time
+    """
+    assert batch_size % inst_per_batch == 0
     while True:
         switches = [np.random.randint(0, 2, size=(n_branch))]
         flipped_branch = np.random.randint(0, n_branch, size=(batch_size - 1))
@@ -94,9 +117,14 @@ def bf_chls(batch_size, n_branch, lds_a_shape, readout_time):
             switches.append(switches[i].copy())
             switches[-1][pos] ^= 1
         switches = np.array(switches)
-        mismatch = np.random.normal(size=(2, *lds_a_shape), loc=1.0, scale=0.1)
-        # Use the same mismatch across all batch
-        mismatch = np.repeat(mismatch[np.newaxis, ...], batch_size, axis=0)
+
+        # Generate inst_per_batch mismatch samples
+        mismatches = np.random.normal(
+            size=(inst_per_batch, 2, *lds_a_shape), loc=1.0, scale=0.1
+        )
+        # Repeat each mismatch samples batch_size // inst_per_batch times
+        # and stack them together
+        mismatch = np.repeat(mismatches, int(batch_size // inst_per_batch), axis=0)
 
         # Sanity check: use different mismatch the i2o score should be 0
         # mismatch = np.random.normal(
@@ -113,6 +141,7 @@ def train(
     dataloader: FunctionType,
     optim: optax.GradientTransformation,
     batch_size: int,
+    inst_per_branch: int,
     steps: int,
     print_every: int,
 ):
@@ -122,7 +151,7 @@ def train(
 
     opt_state = optim.init(eqx.filter(model, eqx.is_array))
     n_branch, lds_a_shape = model.n_branch, model.lds_a_shape
-    loader = dataloader(batch_size, n_branch, lds_a_shape)
+    loader = dataloader(batch_size, inst_per_branch, n_branch, lds_a_shape)
 
     # Always wrap everything -- computing gradients, running the optimiser, updating
     # the model -- into a single JIT region. This ensures things run as fast as
@@ -166,6 +195,8 @@ def train(
             print(f"gm_l:\n{model.gm_l}")
             print(f"gmc_diff:\n{model.gm_c - prev_gmc}")
             print(f"gml_diff:\n{model.gm_l - prev_gml}")
+            print(f"c_val:\n{model.c_val}")
+            print(f"l_val:\n{model.l_val}")
             prev_gmc, prev_gml = model.gm_c, model.gm_l
     return model
 
@@ -177,11 +208,12 @@ N_ORDER = args.n_order
 READOUT_TIME = args.readout_time
 RAND_INIT = args.rand_init
 BATCH_SIZE = args.batch_size
+INST_PER_BRANCH = args.inst_per_branch
 STEPS = args.steps
 PRINT_EVERY = args.print_every
 LOGISTIC_K = args.logistic_k
 
-optim = optax.adamw(LEARNING_RATE, weight_decay=0)
+optim = optax.adam(LEARNING_RATE)
 rand_loader = partial(random_chls_and_mismatch, readout_time=READOUT_TIME)
 bf_loader = partial(bf_chls, readout_time=READOUT_TIME)
 
@@ -236,6 +268,7 @@ i2o_ideal = partial(i2o_score, quantize_fn=shifted_sign)
 #     print(ideal_loss, approx_loss)
 
 print(model.gm_c, model.gm_l)
+print(model.c_val, model.l_val)
 model = train(
     model=model,
     loss=i2o_sigmoid,
@@ -243,6 +276,7 @@ model = train(
     dataloader=bf_loader,
     optim=optim,
     batch_size=BATCH_SIZE,
+    inst_per_branch=INST_PER_BRANCH,
     steps=STEPS,
     print_every=PRINT_EVERY,
 )
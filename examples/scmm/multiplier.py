e3415d692bb780f1608c73e06bbd650eb9926ddf
Yu-Neng Wang <wynwyn@stanford.edu>
2024-03-04 16:30:52 -0800

    [WIP] Try to simulate with actual circuit parameter - What is the sign bit and endian? - The simulation takes forever and the value is far off. Is the problem Gon or step size?

--- examples/scmm/multiplier.py
+++ examples/scmm/multiplier.py
@@ -1,4 +1,5 @@
-import sys
+import argparse
+import re
 from functools import partial
 from types import FunctionType
 
@@ -17,30 +18,158 @@ def weight_to_sequential(w: list[float], clk_period: float) -> FunctionType:
     return partial(sequential_array, arr=w, period=clk_period)
 
 
+def plot_mc_csv(file_path: str, ax: plt.Axes):
+    """
+    Plot the Monte Carlo simulation results from a CSV file.
+
+    The CSV file should have the following format:
+    ```
+    HEADER (Ignored)
+    time, mc_run_1, time, mc_run_2, time, mc_run_3, ...
+    ```
+    Args:
+        file_path (str): The path to the CSV file.
+        ax (plt.Axes): The axis to plot on.
+    """
+    data = np.genfromtxt(file_path, delimiter=",", skip_header=1)
+    time_points = data[:, 0::2].T
+    mc_runs = data[:, 1::2].T
+    for time, run in zip(time_points, mc_runs):
+        ax.plot(time, run)
+    ax.set_xlabel("Time (s)")
+    ax.set_ylabel("Voltage (V)")
+    ax.set_title("Monte Carlo Simulation")
+    return ax
+
+
+def parse_scs(scs_path: str) -> dict:
+    """
+    Parse the SCS file and return the parsed data as a dictionary.
+
+    Args:
+        scs_path (str): The path to the SCS file.
+    Returns:
+        dict: The parsed data.
+    """
+    with open(scs_path, "r") as f:
+        lines = f.readlines()
+
+    mode = "WAIT"
+    data = {"in_val": [], "weight_bit_pos": [], "weight_val": []}
+    for line in lines:
+        if mode == "WAIT":
+            if line.startswith("_vSCM_IN"):
+                mode = "READ_IN"
+            elif line.startswith("//digital weight"):
+                mode = "READ_WEIGHT"
+        elif mode == "READ_IN":
+            vals = line.split()
+            if len(vals) == 1:
+                mode = "WAIT"
+            else:
+                assert vals[1].endswith("n"), "Assume time is in ns!"
+                data["in_val"].append(float(vals[2]))
+        elif mode == "READ_WEIGHT":
+            # Use regex to find data="..." in the line and parse the value
+            data["weight_bit_pos"].append(int(line[7]))
+            match = re.search(r'data="[01]+"', line)
+            if match:
+                data_str = match.group(0).split("=")[1][1:-1]
+                data["weight_val"].append([int(bit) for bit in data_str])
+            else:
+                raise ValueError("Could not find weight bit value in line!")
+
+    # Reorder the weight values based on the bit position
+    data["weight_val"] = [
+        val for _, val in sorted(zip(data["weight_bit_pos"], data["weight_val"]))
+    ]
+    return data
+
+
+def weight_bit_to_val(weight_val: list[list[int]]) -> list[float]:
+    """
+    Convert a list of weight bit values to a list of weight values.
+
+    The weight_val is in the shape of (n_bits, n_weights).
+
+    Args:
+        weight_val (list[list[int]]): The list of weight bit values.
+    Returns:
+        list[float]: The list of weight values.
+    """
+    weight_val_arr = np.array(weight_val, dtype=float)
+    n_bits, n_weights = weight_val_arr.shape
+    weight_val_arr = weight_val_arr * (2 ** np.arange(n_bits)[:, None])
+    weight_val_float = weight_val_arr.sum(axis=0)
+    return weight_val_float
+
+
 if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="Run SCMM example.")
+    parser.add_argument("--vec_len", type=int, help="Length of input vector.")
+    parser.add_argument("--c_ratio", type=float, help="Capacitor ratio.", required=True)
+    parser.add_argument("--c1", type=float, help="Value of C1.", required=True)
+    parser.add_argument("--freq", type=float, help="Value of FREQ.", required=True)
+    parser.add_argument("--g_on", type=float, help="Value of G_ON.", default=1e6)
+    parser.add_argument("--g_off", type=float, help="Value of G_OFF.", default=0.0)
+    parser.add_argument("--r_in", type=float, help="Value of R_IN.", default=0.0)
+    parser.add_argument(
+        "--from_scs", type=str, help="Initialize weight with scs file.", default=None
+    )
+    parser.add_argument(
+        "--plot_csv", type=str, help="Plot mc simulation fomr csv.", default=None
+    )
+    parser.add_argument(
+        "--plot_analytical",
+        action="store_true",
+        help="Plot analytical solution.",
+        default=False,
+    )
+
+    args = parser.parse_args()
+    C_RATIO = args.c_ratio
+    C1 = args.c1
+    C2 = C1 * C_RATIO
+    G_ON = args.g_on
+    G_OFF = args.g_off
+    R_IN = args.r_in
+    FREQ = args.freq
+
+    if args.plot_csv:
+        fig, ax = plt.subplots()
+        plot_mc_csv(args.plot_csv, ax)
+        plt.show()
+        plt.close()
+
+    assert (
+        args.vec_len or args.from_scs
+    ), "Either vec_len or from_scs should be provided!"
+    if args.from_scs:
+        scs_data = parse_scs(args.from_scs)
+        in_val = scs_data["in_val"]
+        weight = scs_data["weight_val"]
+        vec_len = len(in_val)
+        weights = weight_bit_to_val(weight)
+        assert len(weights) == len(in_val), "Input and weight length mismatch!"
+        vin = weight_to_sequential(in_val[:10], 1 / FREQ)
+        c1 = weight_to_sequential(weights[:10], 1 / FREQ)
+        vec_len = len(in_val[:10])
+
+    else:
+        vec_len = args.vec_len
+        in_val = np.random.randint(-4, 5, size=vec_len)
+        weight = np.random.randint(1, 5, size=vec_len) * C1
+
+        vin = weight_to_sequential(in_val, 1 / FREQ)
+        c1 = weight_to_sequential(weight, 1 / FREQ)
+
     system = Ark(cdg_spec=scmm_spec)
 
     Inp = scmm_spec.node_type("InpV")
     CapWeight = scmm_spec.node_type("CapWeight")
     CapSAR = scmm_spec.node_type("CapSAR")
     Sw = scmm_spec.edge_type("Sw")
-
-    # N-path filter implementation
-    C_RATIO = 39
-    C1 = 300e-10
-    C2 = C1 * C_RATIO
-    G_ON = 1e6
-    G_OFF = 0.0
-    R_IN = 1e-9
-    FREQ = 1e9
-
-    vec_len = int(sys.argv[1])
-
-    in_val = np.random.randint(-4, 5, size=vec_len)
-    weight = np.random.randint(1, 5, size=vec_len) * C1
-
-    vin = weight_to_sequential(in_val, 1 / FREQ)
-    c1 = weight_to_sequential(weight, 1 / FREQ)
 
     TIME_RANGE = [0, vec_len / FREQ]
 
@@ -61,10 +190,11 @@ if __name__ == "__main__":
 
     system.print_prog()
 
-    time_points = np.linspace(*TIME_RANGE, 1000)
+    time_points = np.linspace(*TIME_RANGE, 100)
     system.execute(
         cdg=scmm,
         time_eval=time_points,
+        max_step=1 / FREQ / 100,
     )
 
     fig, ax = plt.subplots(nrows=5)
@@ -82,19 +212,20 @@ if __name__ == "__main__":
     plt.show()
     plt.clf()
 
-    readout_point = [i / FREQ for i in range(1, vec_len + 1)]
-    ideal_cumsum = np.cumsum(in_val * weight / C1) / C_RATIO
+    if args.plot_analytical:
+        readout_point = [i / FREQ for i in range(1, vec_len + 1)]
+        ideal_cumsum = np.cumsum(in_val * weight / C1) / C_RATIO
 
-    k = C2 / (C2 + np.abs(weight))
-    mu = weight / C2
-    a_arr = [
-        [mu[j] * np.prod(k[j : i + 1]) for j in range(i + 1)] for i in range(len(k))
-    ]
-    scmm_analytic = [np.sum(a_arr[i] * in_val[: i + 1]) for i in range(len(a_arr))]
+        k = C2 / (C2 + np.abs(weight))
+        mu = weight / C2
+        a_arr = [
+            [mu[j] * np.prod(k[j : i + 1]) for j in range(i + 1)] for i in range(len(k))
+        ]
+        scmm_analytic = [np.sum(a_arr[i] * in_val[: i + 1]) for i in range(len(a_arr))]
 
-    scmm_cumsum = csar.get_trace(n=0)
-    plt.plot(readout_point, ideal_cumsum, label="Ideal", marker="^")
-    plt.plot(readout_point, scmm_analytic, label="SCMM Analytical", marker="o")
-    plt.plot(time_points, scmm_cumsum, label="V_C2")
-    plt.legend()
-    plt.show()
+        scmm_cumsum = csar.get_trace(n=0)
+        plt.plot(readout_point, ideal_cumsum, label="Ideal", marker="^")
+        plt.plot(readout_point, scmm_analytic, label="SCMM Analytical", marker="o")
+        plt.plot(time_points, scmm_cumsum, label="V_C2")
+        plt.legend()
+        plt.show()

Fix randomness bug in gumbel softmax

examples/obc/pattern_recog_dataloader.py
@@ -35,7 +35,8 @@ def dataloader(batch_size: int, n_node: int):
     while True:
         x_init_states = np.random.rand(batch_size, n_node)
         noise_seed = np.random.randint(0, 2**32 - 1, size=batch_size)
-        yield jnp.array(x_init_states), jnp.array(noise_seed)
+        args_seed = np.random.randint(0, 2**32 - 1, size=batch_size)
+        yield jnp.array(x_init_states), jnp.array(args_seed), jnp.array(noise_seed)
 
 
 def dataloader2(
@@ -91,6 +92,7 @@ def dataloader2(
 
         x, y = jnp.array(x), jnp.array(y)
 
+        args_seed = jnp.array(np.random.randint(0, 2**32 - 1, size=batch_size))
         noise_seed = jnp.array(np.random.randint(0, 2**32 - 1, size=batch_size))
         # print(f"loss: {periodic_mse(x, y):.4f}")
-        yield x, noise_seed, y
+        yield x, args_seed, noise_seed, y

examples/obc/pattern_recog_main.py
@@ -144,7 +144,6 @@ def edge_init_to_trainable_init(
         weight_choices: list = Cpl_digital.attr_def["k"].attr_type.val_choices
 
         # normalize the weight choices to between -1 and 1
-        normalize_factor = 2 ** (WEIGHT_BITS - 1)
         weight_choices = np.array(weight_choices)
 
         # If digital setup, the only analog trainable is the lock strength
@@ -166,7 +165,7 @@ def edge_init_to_trainable_init(
     return (a_trainable, d_trainable)
 
 
-# @eqx.filter_jit
+@eqx.filter_jit
 def make_step(
     model: BaseAnalogCkt,
     opt_state: PyTree,
@@ -193,9 +192,9 @@ def plot_evolution(
     gumbel_temp: float,
     hard_gumbel: bool = True,
 ):
-    x_init, noise_seed = data[0], data[1]
-    y_raw = jax.vmap(model, in_axes=(None, 0, None, None, 0, None, None))(
-        time_info, x_init, [], 0, noise_seed, gumbel_temp, hard_gumbel
+    x_init, args_seed, noise_seed = data[0], data[1], data[2]
+    y_raw = jax.vmap(model, in_axes=(None, 0, None, 0, 0, None, None))(
+        time_info, x_init, [], args_seed, noise_seed, gumbel_temp, hard_gumbel
     )
     plot_time = [i for i in range(len(saveat))]
 

tests/test.py
@@ -110,7 +110,6 @@ TestClass = OptCompiler().compile(
     trainable_mgr=trainable_mgr,
     normalize_weight=True,
     do_clipping=False,
-    hard_gumbel=True,
 )
 
 graph.initialize_all_states(0)
@@ -130,7 +129,7 @@ y0 = jnp.array(TestClass.cdg_to_initial_states(graph))
 import matplotlib.pyplot as plt
 
 for i in range(10):
-    a = test(time_info, y0, [1], i, 0, 1)
+    a = test(time_info, y0, [1], i, 1)
 
     plt.plot(a)
 plt.show()
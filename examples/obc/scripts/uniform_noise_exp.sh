19192d012cde6ead23a9ae48578d2f3e09dca2d4
Yu-Neng Wang <wynwyn@stanford.edu>
2025-04-17 20:25:02 -0700

    Update readme, setup.py and 1 experiment script

--- examples/obc/scripts/uniform_noise_exp.sh
+++ examples/obc/scripts/uniform_noise_exp.sh
@@ -7,6 +7,7 @@ lr=1e-1
 trans_noise_std=0.01
 n_class=5
 tag=uniform-noise-exp
+wandb="" # --wandb
 for seed in {0..3}
 do
     for weight_bits in 1 2 3
@@ -22,20 +23,20 @@ do
                     run_name=bit$weight_bits-$weight_init-seed$seed$tl$tc
                     save_path=$dir/$run_name.npz
                     python3 pattern_recog_main.py --n_class $n_class --diff_fn $diff_fn  --uniform_noise --vectorize \
-                    --trans_noise_std $trans_noise_std --steps $steps --bz $bz --lr $lr --optimizer $optimizer --seed $seed  --wandb --tag $tag \
+                    --trans_noise_std $trans_noise_std --steps $steps --bz $bz --lr $lr --optimizer $optimizer --seed $seed  $wandb --tag $tag \
                     --weight_bits $weight_bits --gumbel_temp_start 10.0 --gumbel_temp_end 1.0 --gumbel_schedule exp  \
                     $tc $tl --pattern_shape 10x6 --save_weight $save_path --weight_init $weight_init --run_name $run_name --no_noiseless
                     python3 pattern_recog_main.py --n_class $n_class --diff_fn $diff_fn  --uniform_noise --vectorize \
-                    --trans_noise_std $trans_noise_std --steps $steps --bz $bz --optimizer $optimizer --seed $seed --wandb --tag $tag \
+                    --trans_noise_std $trans_noise_std --steps $steps --bz $bz --optimizer $optimizer --seed $seed $wandb --tag $tag \
                     --weight_bits $weight_bits $tc $tl --pattern_shape 10x6 --load_weight $save_path --weight_init $weight_init \
                     --run_name $run_name-test --test --no_noiseless
                 done
             done
         done
-        Test the loss w/ only hebbian rule
+        # Test the loss w/ only hebbian rule
         run_name=bit$weight_bits-$weight_init-seed$seed-no-opt
         python3 pattern_recog_main.py --n_class $n_class --diff_fn $diff_fn  --uniform_noise --vectorize \
-        --trans_noise_std $trans_noise_std --steps $steps --bz $bz --optimizer $optimizer --seed $seed --wandb --tag $tag-baseline \
+        --trans_noise_std $trans_noise_std --steps $steps --bz $bz --optimizer $optimizer --seed $seed $wandb --tag $tag-baseline \
         --weight_bits $weight_bits $tc $tl --pattern_shape 10x6 --weight_init hebbian \
         --run_name $run_name-test --test --no_noiseless
     done

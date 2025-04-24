a4695028b1e61cbb0151f844c217c2ab5d7946ab
Yu-Neng Wang <wynwyn@stanford.edu>
2025-04-23 19:01:13 -0700

    [WIP] Add obc+tln example

--- examples/nacs_as_nn/spec.py
+++ examples/nacs_as_nn/spec.py
@@ -1,8 +1,10 @@
+import jax.numpy as jnp
+
 from ark.specification.attribute_def import AttrDef, AttrDefMismatch
 from ark.specification.attribute_type import AnalogAttr, FunctionAttr
 from ark.specification.cdg_types import EdgeType, NodeType
 from ark.specification.production_rule import ProdRule
-from ark.specification.rule_keyword import DST, EDGE, SELF, SRC, VAR
+from ark.specification.rule_keyword import DST, EDGE, SELF, SRC, TIME, VAR
 from ark.specification.specification import CDGSpec
 
 # Cellular nonlinear network
@@ -15,6 +17,12 @@ obc_spec = CDGSpec("obc")
 # https://journals.aps.org/prl/abstract/10.1103/PhysRevLett.61.259
 # dh_i/dt = -h_i + sum_j J_ij tanh(g*h_j)
 cann_spec = CDGSpec("cann")
+
+# Transmission line network
+tln_spec = CDGSpec("tln")
+
+# Mix obc and tln
+obc_tln_spec = CDGSpec("obc-tln")
 
 # Input that is fixed in the dynamic
 # should be stateless, setting for convenience of setting its value
@@ -167,3 +175,89 @@ production_rules = [n_cp_dst, cp_self, inp_cp_neuron]
 cann_spec.add_production_rules(production_rules)
 #### Production rules end ####
 # ------- NNL specification end -------
+
+
+# ------- TLN specification -------
+#### Type definitions start ####
+# Parallel capacitor(c=capacitance) and resistor(g=conductance)
+IdealV = NodeType(
+    name="IdealV",
+    attrs={
+        "order": 1,
+        "attr_def": {
+            "c": AttrDef(attr_type=AnalogAttr()),
+            "g": AttrDef(attr_type=AnalogAttr()),
+        },
+    },
+)
+# Series inductor(l=inductance) and resistor(r=resistance)
+IdealI = NodeType(
+    name="IdealI",
+    attrs={
+        "order": 1,
+        "attr_def": {
+            "l": AttrDef(attr_type=AnalogAttr()),
+            "r": AttrDef(attr_type=AnalogAttr()),
+        },
+    },
+)
+
+IdealE = EdgeType(name="IdealE")
+tln_spec.add_cdg_types([IdealV, IdealI, IdealE])
+#### Type definitions end ####
+
+#### Production rules start ####
+_v2i = ProdRule(IdealE, IdealV, IdealI, SRC, -VAR(DST) / SRC.c)
+v2_i = ProdRule(IdealE, IdealV, IdealI, DST, VAR(SRC) / DST.l)
+_i2v = ProdRule(IdealE, IdealI, IdealV, SRC, -VAR(DST) / SRC.l)
+i2_v = ProdRule(IdealE, IdealI, IdealV, DST, VAR(SRC) / DST.c)
+vself = ProdRule(IdealE, IdealV, IdealV, SELF, -VAR(SRC) * SRC.g / SRC.c)
+iself = ProdRule(IdealE, IdealI, IdealI, SELF, -VAR(SRC) * SRC.r / SRC.l)
+prod_rules = [_v2i, v2_i, _i2v, i2_v, vself, iself]
+tln_spec.add_production_rules(prod_rules)
+#### Production rules end ####
+# ------- TLN specification end -------
+
+# ------- TLN+OBC specification -------
+obc_tln_spec.add_cdg_types(tln_spec.cdg_types() + obc_spec.cdg_types())
+obc_tln_spec.add_production_rules(
+    tln_spec.production_rules() + obc_spec.production_rules()
+)
+
+# Convert wave from phase to voltage
+Phase2VoltE = EdgeType(
+    name="phase2vE",
+    attrs={
+        "attr_def": {
+            "f": AttrDef(attr_type=AnalogAttr()),
+            "sin": AttrDef(attr_type=FunctionAttr(nargs=1)),
+        }
+    },
+)
+osc2v = ProdRule(
+    Phase2VoltE,
+    Osc,
+    IdealV,
+    DST,
+    EDGE.sin(TIME * 2 * jnp.pi * EDGE.f + VAR(SRC) * jnp.pi) / DST.c,
+)
+
+# Rectifier for wave
+RectifyE = EdgeType(
+    name="rectifyE",
+    attrs={
+        "attr_def": {
+            "abs": AttrDef(attr_type=FunctionAttr(nargs=1)),
+        }
+    },
+)
+v_rect_v = ProdRule(
+    RectifyE,
+    IdealV,
+    IdealV,
+    DST,
+    EDGE.abs(VAR(SRC)) / DST.c,
+)
+obc_tln_spec.add_cdg_types([Phase2VoltE, RectifyE])
+obc_tln_spec.add_production_rules([osc2v, v_rect_v])
+# ------- TLN+OBC specification end -------

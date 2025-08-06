587d19355354ffbaab89867ab38ce0d404757482
Yu-Neng Wang <33870148+WangYuNeng@users.noreply.github.com>
2025-08-06 11:28:39 -0700

    Update edge_detection.sh

--- examples/cnn/scripts/edge_detection.sh
+++ examples/cnn/scripts/edge_detection.sh
@@ -14,6 +14,7 @@ for seed in {0..5}
 do
     # Test the model
     # seed+=4
+    save_path=$dir/$seed.npz
     python edge_detection_main.py --dataset silhouettes --mismatched_edge 0.1 --end_time 1.5 \
     --bz 128 --seed $((seed+4)) --steps 2 --mismatched_node --vectorize \
     --load_weight $save_path --tag $tag --test --wandb
@@ -22,7 +23,6 @@ done
 # No vectorization
 for seed in {0..0}
 do
-    save_path=$dir/$seed.npz
     python edge_detection_main.py --plot_evolve 5 --dataset silhouettes --mismatched_edge 0.1 --end_time 1.5 \
     --bz 128 --seed $seed --steps 1  --lr 0.1 --mismatched_node  \
     --tag $tag --wandb

87d593a31692bdf3bc7b995dd618d8686a9426ea
Yu-Neng Wang <wynwyn@stanford.edu>
2025-08-07 13:01:46 -0700

    Update blackbox opt script for a fair comparison

--- examples/cnn/scripts/blackbox.sh
+++ examples/cnn/scripts/blackbox.sh
@@ -6,7 +6,7 @@ for seed in {0..5}
 do
     save_path=$dir/blackbox$seed.npz
     python edge_detection_main.py --plot_evolve 5 --dataset silhouettes --mismatched_edge 0.1 --end_time 1.5 \
-    --bz 128 --seed $seed --steps 2  --mismatched_node --vectorize \
+    --bz 128 --seed $seed --steps 1  --mismatched_node --vectorize \
     --save_weight $save_path --tag $tag --wandb --blackbox_opt ax
 
     python edge_detection_main.py --dataset silhouettes --mismatched_edge 0.1 --end_time 1.5 \
@@ -18,7 +18,7 @@ for seed in {0..5}
 do
     save_path=$dir/blackbox-lim$seed.npz
     python edge_detection_main.py --plot_evolve 5 --dataset silhouettes --mismatched_edge 0.1 --end_time 1.5 \
-    --bz 128 --seed $seed --steps 2  --mismatched_node --vectorize \
+    --bz 128 --seed $seed --steps 1  --mismatched_node --vectorize \
     --save_weight $save_path --tag $tag --wandb --blackbox_opt ax --limited_range
 
     python edge_detection_main.py --dataset silhouettes --mismatched_edge 0.1 --end_time 1.5 \
--- examples/tln/blackbox_opt.sh
+++ examples/tln/blackbox_opt.sh
@@ -9,7 +9,7 @@ do
     chl_per_bit=32
     lr="5e-3"
     python3 train_w_optcompile.py --seed $seed --readout_time $readout_time \
-    --inst_per_batch 8 --chl_per_bit $chl_per_bit --steps 128  \
+    --inst_per_batch 8 --chl_per_bit $chl_per_bit --steps 24  \
     --n_branch 32 --n_time_point $n_time_point --normalize --wandb --tag $tag --save_weight $dir/blackbox$seed \
     --vectorize --blackbox_opt ax
 done

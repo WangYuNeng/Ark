f27bea716e5bad288a7e6f0394949060d24275ee
Yu-Neng Wang <wynwyn@stanford.edu>
2025-08-07 13:40:17 -0700

    Update tln opt script

--- examples/tln/blackbox_opt.sh
+++ examples/tln/blackbox_opt.sh
@@ -8,22 +8,17 @@ do
     n_time_point=100
     chl_per_bit=32
     lr="5e-3"
+    save_path=$dir/blackbox$seed.npz
     python3 train_w_optcompile.py --seed $seed --readout_time $readout_time \
     --inst_per_batch 8 --chl_per_bit $chl_per_bit --steps 24  \
-    --n_branch 32 --n_time_point $n_time_point --normalize --wandb --tag $tag --save_weight $dir/blackbox$seed \
+    --n_branch 32 --n_time_point $n_time_point --normalize --wandb --tag $tag --save_weight $save_path \
     --vectorize --blackbox_opt ax
-done
 
-
-# Testing
-for seed in {0..10};
-do
-    readout_time="10e-9"
-    n_time_point=100
-    chl_per_bit=32
-    lr="5e-3"
+    # Testing
     python3 train_w_optcompile.py --seed $((seed+444)) --readout_time $readout_time \
     --inst_per_batch 8 --chl_per_bit $chl_per_bit --steps 24  \
-    --n_branch 32 --n_time_point $n_time_point --normalize --wandb --tag $tag --load_weight $dir/blackbox$seed \
+    --n_branch 32 --n_time_point $n_time_point --normalize --wandb --tag $tag --load_weight $save_path \
     --vectorize --test
 done
+
+

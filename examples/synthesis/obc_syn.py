46eff5e84e048bbe17d928fb4e479c46f3cd2594
Yu-Neng Wang <wynwyn@stanford.edu>
2025-10-14 16:09:17 -0700

    Add beta fitting

--- examples/synthesis/obc_syn.py
+++ examples/synthesis/obc_syn.py
@@ -1,8 +1,10 @@
 # Demonstrate syntheizing function with oscillators
 
+from cmath import phase
 from itertools import product
 from typing import Callable, Iterable
 
+import cvxpy as cp
 import matplotlib.pyplot as plt
 import numpy as np
 from cvc5.pythonic import Const, Int, Ints, Or, Real, Reals, RealVal, Solver, sat
@@ -31,6 +33,53 @@ def phase_to_bool(p):
         raise ValueError("Input must be int, float, list, or np.ndarray")
 
 
+def fit_temperature(
+    phase_to_energy: dict[tuple, int], phase_to_measurements: dict[tuple, int]
+):
+    """Fit an temperature parameter from calculated energy and measured distribution.
+
+    Args:
+        phase_to_energy (dict[tuple, int]): Mapping from state (tuple of phases) to energy value
+        phase_to_measurements (dict[tuple, int]): Mapping from state (tuple of phases) to count of occurrences
+    Returns:
+        float: Fitted temperature parameter beta
+        float: KL divergence value
+        dict[tuple, int]: Fitted distribution over states
+    """
+
+    assert set(phase_to_energy.keys()) == set(
+        phase_to_measurements.keys()
+    ), "Energy and measurement keys must match"
+    n_measurement = sum(phase_to_measurements.values())
+    phase_to_probs = {k: v / n_measurement for k, v in phase_to_measurements.items()}
+
+    beta = cp.Variable(1)
+    kl_div = 0
+
+    # Minimize KL divergence between the true distribution (prob) and the fitting distribution
+    log_Q_denominator = cp.log_sum_exp(
+        cp.vstack([-beta * e for e in phase_to_energy.values()])
+    )
+    for phases in phase_to_energy.keys():
+        energy = phase_to_energy[phases]
+        prob = phase_to_probs[phases]
+        log_Q_numerator = -beta * energy
+        if prob != 0:
+            kl_div += prob * (cp.log(prob) - (log_Q_numerator - log_Q_denominator))
+    prob = cp.Problem(cp.Minimize(kl_div))
+    prob.solve()
+    fitted_beta = beta.value[0]
+    fitted_distribution = {
+        k: np.exp(-fitted_beta * e) for k, e in phase_to_energy.items()
+    }
+    partition = sum(fitted_distribution.values())
+    fitted_distribution = {
+        k: v / partition * n_measurement for k, v in fitted_distribution.items()
+    }
+
+    return fitted_beta, prob.value, fitted_distribution
+
+
 def random_simulation(
     J: np.ndarray,
     n_sim=256,
@@ -38,7 +87,6 @@ def random_simulation(
     Kl=1,
     super_harmonic=2,
     t_span=(0, 2),
-    dt=0.01,
     anneal=False,
 ):
     """Simulate Kuramoto model with random initial states
@@ -50,7 +98,6 @@ def random_simulation(
         Kl (int, optional): Injection locking scale factor. Defaults to 1.
         super_harmonic (int, optional): Injectio locking frequency. Defaults to 2.
         t_span (tuple, optional): Simulation time span. Defaults to (0, 2).
-        dt (float, optional): Initial time step. Defaults to 0.01.
         anneal (bool, optional): Whether to use an annealing schedule for locking. Defaults to False.
 
     Returns:
@@ -154,7 +201,7 @@ def synthesize_general(
     # NxN coupling matrix
     # J = Ints(" ".join([f"J{i}{j}" for i in range(n_osc) for j in range(n_osc)]))
     J = Ints(" ".join([f"J{i}{j}" for i in range(n_osc) for j in range(n_osc)]))
-    J_max = np.array(J).reshape((n_osc, n_osc))
+    J_mat = np.array(J).reshape((n_osc, n_osc))
 
     # Auxiliary variable: minimum energy
     e_min = Int("e_min")
@@ -173,7 +220,7 @@ def synthesize_general(
         non_solution_states = []
         for free_vals in aux_val_tab:
             v = np.array(ios_phase + free_vals + fixed_oscs)
-            e = energy_fn(J_max, v, exclude_ref_energy=exclude_ref_energy)
+            e = energy_fn(J_mat, v, exclude_ref_energy=exclude_ref_energy)
             if logic_fn(*ios_bool):
                 # Solution states have energy at least e_min
                 possible_solution_states.append(e)
@@ -234,13 +281,26 @@ def synthesize_general(
         # List the truth table and corresponding energies
         print("Truth table and energies:")
         titles = ["I/O", "E_w_ref", "E_wo_ref"]
+        phase_to_energy_w_ref, phase_to_energy_wo_ref = {}, {}
         print(f"{titles[0]:<20}{titles[1]:<10}{titles[2]:<10}")
         for ios_phase in io_val_tab:
             es_w_ref, es_wo_ref = [], []
             for aux_vals in aux_val_tab:
                 v = np.array(ios_phase + aux_vals + fixed_oscs)
-                es_w_ref.append(m[energy_fn(J_max, v, exclude_ref_energy=False)])
-                es_wo_ref.append(m[energy_fn(J_max, v, exclude_ref_energy=True)])
+                energy_w_ref = m[
+                    energy_fn(J_mat, v, exclude_ref_energy=False)
+                ].as_long()
+                energy_wo_ref = m[
+                    energy_fn(J_mat, v, exclude_ref_energy=True)
+                ].as_long()
+                es_w_ref.append(energy_w_ref)
+                es_wo_ref.append(energy_wo_ref)
+                phase_to_energy_w_ref[tuple(phase_to_bool(ios_phase + aux_vals))] = (
+                    energy_w_ref
+                )
+                phase_to_energy_wo_ref[tuple(phase_to_bool(ios_phase + aux_vals))] = (
+                    energy_wo_ref
+                )
             ios_bool = phase_to_bool(ios_phase)
             print(f"{str(ios_bool):<20}{str(es_w_ref):<10} {str(es_wo_ref):<10}")
 
@@ -250,7 +310,9 @@ def synthesize_general(
             .reshape((n_osc, n_osc))
             .astype(float)
         )
-        sim_results = random_simulation(J_val, n_sim=2**10, Kl=1, Kc=1, anneal=False)
+        sim_results = random_simulation(
+            J_val, n_sim=2 ** (n_osc + 4), Kl=1, Kc=1, anneal=False, t_span=(0, 2)
+        )
 
         # Plot the sim result histogram -- count how many times each input/output combination occurs
         hist = {
@@ -266,6 +328,21 @@ def synthesize_general(
         plt.bar(
             range(len(hist)), hist.values(), tick_label=[str(k) for k in hist.keys()]
         )
+
+        for label, p2e, color in zip(
+            ["w/ Ref energy", "w/o Ref energy"],
+            [phase_to_energy_w_ref, phase_to_energy_wo_ref],
+            ["orange", "gold"],  # contrast to blue bars
+        ):
+            beta, kl_div, distribution = fit_temperature(p2e, hist)
+            # Plot the fitted distribution as a line on top of the histogram
+            plt.plot(
+                range(len(hist)),
+                [distribution[k] for k in hist.keys()],
+                label=f"{label}, beta={beta:.2f}, KL Div={kl_div:.4f}",
+                marker="o",
+                color=color,
+            )
         # Highlight the valid states
         valid_states = [
             tuple(phase_to_bool(ios_phase))
@@ -281,6 +358,7 @@ def synthesize_general(
         plt.ylabel("Count")
         plt.title("Final State Distribution")
         plt.tight_layout()
+        plt.legend()
         plt.show()
 
 
